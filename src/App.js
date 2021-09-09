3d2c04b00c3d49f12dd74664a7c3775cd20c2221
Ritwik <shawritwik2@gmail.com>
2021-09-09 12:29:23 +0530

    Added extra funtionalities to TextUtils

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import "./App.css";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar title= "TextUtils" aboutText= "About Us"/>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyse below:"/>
+        {/* <About/> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,19 +13,52 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = ()=>{
+        //console.log("Uppercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleClr = ()=>{        
+        let newText = ("");
+        setText(newText);
+    }
+
+    const handleCopy = ()=>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
 
     // const [count, setCount] = useState(0);   HOOKS   DECLARATION OF A NEW STATE VARIABLE, WHICH WE'LL CALL "count" and setCount function will be used to update the text
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // text = "New Text";
     // setText("new text");
     return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>             
         <div className="mb-3">             
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>CONVERT TO UPPERCASE</button>
-
+        <button className="btn btn-success mx-2 my-2" onClick={handleUpClick}>CONVERT TO UPPERCASE</button>
+        <button className="btn btn-warning mx-2 my-2" onClick={handleLoClick}>CONVERT TO LOWERCASE</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleClr}>CLEAR TEXT</button>
+        <button className="btn btn-info mx-2 my-2" onClick={handleCopy}>COPY TEXT</button>
+        <button className="btn btn-secondary mx-2 my-2" onClick={handleExtraSpaces}>REMOVE EXTRA SPACE</button>
     </div>
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
     )
 }
